67caa3b54d68c82a56e5a20cb14aff8a97107ed7
mnouwens <mnouwens@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2009-11-21 13:20:34 +0000

    ds
    git-svn-id: file:///var/svn/easyenterprise@32 0db79848-0afd-4259-9b79-6da5ec97fbb6

---
+++ CatalogXS/trunk/CatalogXSBusinessLogic/source/agilexs/catalogxs/businesslogic/CatalogBean.java
@@ -0,0 +1,45 @@
+package agilexs.catalogxs.businesslogic;
+
+import java.io.IOException;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.store.LockObtainFailedException;
+
+import agilexs.catalogxs.jpa.catalog.Product;
+
+@Stateless
+public class CatalogBean extends CatalogBeanBase implements Catalog {
+
+	private final static org.apache.log4j.Logger LOGGER = eu.future.earth.logging.ExtendedLog.getLogger(CatalogBean.class);
+
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public Product updateProduct(Product oldProduct, Product newProduct) {
+		Product result = super.updateProduct(oldProduct, newProduct);
+		LuceneHelper helper = new LuceneHelper();
+		try {
+			IndexWriter writer = helper.getWriter();
+			ProductAnalyzer analyzer = new ProductAnalyzer();
+			Document doc = analyzer.createDocument(result);
+			if (oldProduct != null) {
+				writer.updateDocument(analyzer.createKey(result), doc);
+			} else {
+				writer.addDocument(doc);
+			}
+			writer.optimize();
+			writer.close();
+		} catch (CorruptIndexException e) {
+			LOGGER.error("Corrupted Index.", e);
+		} catch (LockObtainFailedException e) {
+			LOGGER.error("Could not get lock.", e);
+		} catch (IOException e) {
+			LOGGER.error("File Error.", e);
+		}
+		return result;
+	}
+}
