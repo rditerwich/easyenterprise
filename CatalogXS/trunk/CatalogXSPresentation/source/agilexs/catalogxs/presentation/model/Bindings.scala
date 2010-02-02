abe42423425cea53032b17bfa98d257c2f86901e
ruud.diterwich <ruud.diterwich@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2010-02-02 11:10:23 +0000

    git-svn-id: file:///var/svn/easyenterprise@62 0db79848-0afd-4259-9b79-6da5ec97fbb6

---
+++ CatalogXS/trunk/CatalogXSPresentation/source/agilexs/catalogxs/presentation/model/Bindings.scala
@@ -0,0 +1,38 @@
+package agilexs.catalogxs.presentation.model
+
+import net.liftweb.util.Helpers._
+import scala.xml.{NodeSeq, Text, SpecialNode} 
+import agilexs.catalogxs.jpa.catalog._
+
+object Bindings {
+
+    def promotionBindings(p: Promotion)(xhtml: NodeSeq) : NodeSeq = {
+	  p match {
+        case p : VolumeDiscountPromotion =>
+        	bind("promotion", xhtml,           
+              "id" -> Text("VOLDISCPROMO_ID_X"),
+              "start-date" -> Text(p.getStartDate.formatted("YYYY")),
+              "end-date" -> Text(p.getStartDate.formatted("YYYY/mm/dd")),
+              "price" -> Text(p.getPrice.toString),
+              "currency" -> Text(p.getPriceCurrency.toString),
+              "volume-discount" -> Text(p.getVolumeDiscount.toString),
+              "product" -> (xhtml => productBindings(p.getProduct)(xhtml)))
+        case _ => Seq.empty 
+      }
+  }
+  
+  def productBindings(p: Product)(xhtml: NodeSeq) : NodeSeq = {
+    bind("product", xhtml, 
+    	"id" -> Text(p.getId.toString),
+    	"properties" -> (xhtml => propertyValueBindings(null)(xhtml)))
+  }
+  
+  def propertyValueBindings(p: PropertyValue)(xhtml: NodeSeq) : NodeSeq = {
+    bind("property", xhtml,
+         "id" -> Text(p.getId.toString),
+         "name" -> Text(p.getProperty.getName),
+         "label" -> Text(p.getProperty.getName)
+    )
+  }
+
+}
--- CatalogXS/trunk/CatalogXSPresentation/source/agilexs/catalogxs/presentation/model/Model.scala
+++ CatalogXS/trunk/CatalogXSPresentation/source/agilexs/catalogxs/presentation/model/Model.scala
@@ -1,6 +1,6 @@
 package agilexs.catalogxs.presentation.model
 
-import scala.collection.jcl.{BufferWrapper,SetWrapper} 
+import scala.collection.jcl.{BufferWrapper,SetWrapper,IterableWrapper} 
 import javax.persistence.{EntityManager,Persistence}
 
 object Model {
@@ -12,4 +12,5 @@ object Model {
 
   implicit def setToWrapper[A](set : java.util.Set[A]) = new SetWrapper[A]{override def underlying = set}
   implicit def listToWrapper[A](list : java.util.List[A]) = new BufferWrapper[A]{override def underlying = list}
+  implicit def collectionToWrapper[A](collection : java.util.Collection[A]) = new IterableWrapper[A]{override def underlying = collection}
 }
