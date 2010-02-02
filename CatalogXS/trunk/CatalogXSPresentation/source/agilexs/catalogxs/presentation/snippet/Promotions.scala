e06a037c6467f000d9bb234097c21774704bd2f4
ruud.diterwich <ruud.diterwich@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2010-02-02 11:10:12 +0000

    git-svn-id: file:///var/svn/easyenterprise@61 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- CatalogXS/trunk/CatalogXSPresentation/source/agilexs/catalogxs/presentation/snippet/Promotions.scala
+++ CatalogXS/trunk/CatalogXSPresentation/source/agilexs/catalogxs/presentation/snippet/Promotions.scala
@@ -2,36 +2,17 @@ package agilexs.catalogxs.presentation.snippet
 
 import net.liftweb.util.Helpers._
 
-import scala.xml.{NodeSeq, Text, SpecialNode} 
-import scala.util.logging.ConsoleLogger 
-import agilexs.catalogxs.presentation.model.Model
+import scala.xml.NodeSeq 
+import agilexs.catalogxs.presentation.model.Bindings
+import agilexs.catalogxs.presentation.model.Model._
 import agilexs.catalogxs.jpa.catalog._
 
 class Promotions extends BasicSnippet[Promotion] {
 
+  lazy val catalogBean = lookupCatalog()
+
   def all(xhtml: NodeSeq) : NodeSeq = {
-      val catalogBean = lookupCatalog()
-      val promotions = Model.listToWrapper(catalogBean.findAllPromotions(0, 9).asInstanceOf[java.util.List[Promotion]])
-      promotions.flatMap(promotionBindings(_)(xhtml))
-  }
-  
-  def promotionBindings(p: Promotion)(xhtml: NodeSeq) : NodeSeq = {
-	  p match {
-        case p : VolumeDiscountPromotion =>
-        	bind("promotion", xhtml,           
-              "id" -> Text("VOLDISCPROMO_ID_X"),
-              "start-date" -> Text(p.getStartDate.formatted("YYYY")),
-              "end-date" -> Text(p.getStartDate.formatted("YYYY/mm/dd")),
-              "price" -> Text(p.getPrice.toString),
-              "currency" -> Text(p.getPriceCurrency.toString),
-              "volume-discount" -> Text(p.getVolumeDiscount.toString),
-              "product" -> (xhtml => productBindings(p.getProduct)(xhtml)))
-        case _ => Seq.empty 
-      }
-  }
-  
-  def productBindings(p: Product)(xhtml: NodeSeq) : NodeSeq = {
-    bind("product", xhtml, 
-    	"id" -> Text(p.getId.toString))
+      val promotions = catalogBean.findAllPromotions(0, 9) 
+      promotions flatMap (Bindings.promotionBindings(_)(xhtml))
   }
 }  
