dbf39b1a7ac9cf437517134b27a408e465aa8a97
ruud.diterwich <ruud.diterwich@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2010-02-03 06:22:31 +0000

    git-svn-id: file:///var/svn/easyenterprise@65 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- CatalogXS/trunk/CatalogXSPresentation/source/agilexs/catalogxs/presentation/snippet/Catalog.scala
+++ CatalogXS/trunk/CatalogXSPresentation/source/agilexs/catalogxs/presentation/snippet/Catalog.scala
@@ -4,18 +4,19 @@ import net.liftweb.util.Helpers._
 
 import scala.xml.NodeSeq 
 import agilexs.catalogxs.jpa.catalog._
-import agilexs.catalogxs.presentation.model.{CatalogBindings, CatalogCache}
+import agilexs.catalogxs.presentation.model.CatalogCache
+import agilexs.catalogxs.presentation.model.CatalogBindings._
 import agilexs.catalogxs.presentation.model.Conversions._
 
-class Promotions extends BasicSnippet[Promotion] {
+class Catalog extends BasicSnippet[Promotion] {
 
   lazy val catalogBean = lookupCatalog()
 
-  def all(xhtml: NodeSeq) : NodeSeq = {
+  def listPromotions(xhtml: NodeSeq) : NodeSeq = {
     
 	  // TODO store in global or session space somewhere
 	  val catalog = CatalogCache("staples", "webshop", "en")
     
-      catalog.promotions flatMap (CatalogBindings.promotionBindings(_)(xhtml))
+      catalog.promotions flatMap (promotionBindings(_)(xhtml))
   }
 }  
