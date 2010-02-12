9cdf15ffd8540127e5857a2cb4f08577f88dd2cb
hilbrand.bouwkamp <hilbrand.bouwkamp@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2010-02-12 10:57:01 +0000

    In Shopping cart correctly show product information and for demo added real product when reloading page. scala code contains some non working methods for adding products to the shopping cart.
    git-svn-id: file:///var/svn/easyenterprise@103 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- CatalogXS/trunk/CatalogXSPresentation/source/agilexs/catalogxs/presentation/snippet/ShoppingCart.scala
+++ CatalogXS/trunk/CatalogXSPresentation/source/agilexs/catalogxs/presentation/snippet/ShoppingCart.scala
@@ -15,11 +15,12 @@ import Helpers._
 
 import scala.xml.{NodeSeq, Text} 
 
-import agilexs.catalogxs.presentation.model.Model
+import agilexs.catalogxs.presentation.model._
 import agilexs.catalogxs.presentation.model.Conversions._
 import agilexs.catalogxs.jpa.catalog._
 import agilexs.catalogxs.businesslogic.CatalogBean
 import agilexs.catalogxs.jpa.{order => jpa}
+import agilexs.catalogxs.presentation.util.Util
 
 //TODO format money
 //TODO show correct currency sign
@@ -27,6 +28,7 @@ import agilexs.catalogxs.jpa.{order => jpa}
 class ShoppingCart {
   //Session variable that contains the items in the shopping cart during session
   object shoppingCart extends SessionVar[jpa.Order](new jpa.Order)
+  //object orderStatusID extends SessionVar[Box[Long]](Empty)
 
   /**
    * Method to bind to order_status template to display total number of 
@@ -45,6 +47,11 @@ class ShoppingCart {
              totalArticles.toString))
     }
     inner()
+  }
+
+  def redrawOrderStatus() : JsCmd = {
+    //SetHtml(orderStatusID, inner)
+    null;
   }
 
   /**
@@ -66,6 +73,22 @@ class ShoppingCart {
     }
   }
 
+  def add(xhtml: NodeSeq) = {
+    val p = Model.currentProduct orNull
+    val o = new jpa.ProductOrder()
+    o.setProduct(p.delegate)
+    o.setVolume(1)
+
+    def inner(reDraw: () => JsCmd)(xhtml: NodeSeq) : NodeSeq = {
+      //val price = p.propertiesByName.get("Price")
+      //o.setPrice(price.toLong)
+      bind("sc", xhtml,
+           "add" -> ajaxButton(Text("Add to Cart"), () => {
+                  shoppingCart.getProductOrders.add(o);reDraw()}))
+    }
+    inner(redrawOrderStatus)(xhtml)
+  }
+
   /**
    * Within the show phase template 2 templates options are available. One in
    * case no items are in the shopping cart <code>empty</code> and one if there
@@ -85,7 +108,9 @@ class ShoppingCart {
     //FIXME: remove test code from release
     val po = new jpa.ProductOrder
     po.setVolume(10)
-    po.setPrice(20.00)
+    po.setPrice(10 *
+       Model.catalog.productsById(5001).propertiesByName("Price").valueAsString.toDouble)
+    po.setProduct(Model.catalog.productsById(5001).delegate)
     shoppingCart.getProductOrders.add(po)
 
     def inner(): NodeSeq = {
@@ -98,27 +123,27 @@ class ShoppingCart {
            val nhtml = chooseTemplate("show", "items", xhtml);
            bind("item", nhtml,
                 "list" -> list(reDraw)(chooseTemplate("item", "list", nhtml)),
-                "totalPrice" -> <span>&euro; {totalPrice}</span>)
+                "totalPrice" -> <b>&euro; {totalPrice}</b>)
       }
     }
     inner()
   }
 
   def list(reDraw: () => JsCmd)(xhtml : NodeSeq) : NodeSeq = {
-    shoppingCart.getProductOrders.flatMap(productOrder =>
+    shoppingCart.getProductOrders.flatMap(productOrder => {
       bind("shoppingcart", xhtml,
           "deleteButton" -> 
             ajaxButton(Text("delete"),
               () => {shoppingCart.getProductOrders.remove(productOrder); 
                reDraw()}),
-          "articleNumber" -> Text("articleNumber"),
-          "productDescription" -> Text("productDescription"),
-          "pricePerProduct" -> Text("pricePerProduct"),
+          "product" -> 
+            CatalogBindings.productBinding(
+              Model.catalog.productsById(productOrder.getProduct().getId().longValue())).bind("product", chooseTemplate("shoppingcart", "product", xhtml)),
           "volume" -> SHtml.text(
               productOrder.getVolume.toString, 
               { v => productOrder.setVolume(Integer.valueOf(v))},
               "maxlength" -> "3", "style" -> "width:30px;margin:0 5px;"),
-          "priceTotal" -> <b>&euro; {productOrder.getPrice}</b>)) 
+          "priceTotal" -> <span>&euro; {productOrder.getPrice}</span>)}) 
   }
 
   private def totalArticles : Int = {
@@ -126,6 +151,6 @@ class ShoppingCart {
   }
 
   private def totalPrice : Double = {
-        (0.0 /: shoppingCart.getProductOrders.map(_.getPrice.doubleValue)) (_ + _) 
-    }
+    (0.0 /: shoppingCart.getProductOrders.map(_.getPrice.doubleValue)) (_ + _) 
+  }
 }
