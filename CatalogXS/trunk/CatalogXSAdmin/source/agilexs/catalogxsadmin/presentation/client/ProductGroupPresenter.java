f68f1c27caf8beeac7e2a5bbc4474dd17a5f7fdb
hilbrand.bouwkamp <hilbrand.bouwkamp@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2010-02-26 15:07:07 +0000

    First code for displaying productgroup as tree in productgroup admin page.
    git-svn-id: file:///var/svn/easyenterprise@125 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- CatalogXS/trunk/CatalogXSAdmin/source/agilexs/catalogxsadmin/presentation/client/ProductGroupPresenter.java
+++ CatalogXS/trunk/CatalogXSAdmin/source/agilexs/catalogxsadmin/presentation/client/ProductGroupPresenter.java
@@ -1,6 +1,24 @@
 package agilexs.catalogxsadmin.presentation.client;
 
+import java.util.HashMap;
+import java.util.List;
+
+import agilexs.catalogxsadmin.presentation.client.catalog.CatalogView;
+import agilexs.catalogxsadmin.presentation.client.catalog.ProductGroup;
+import agilexs.catalogxsadmin.presentation.client.catalog.PropertyValue;
 import agilexs.catalogxsadmin.presentation.client.page.Presenter;
+import agilexs.catalogxsadmin.presentation.client.services.CatalogServiceAsync;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.OpenEvent;
+import com.google.gwt.event.logical.shared.OpenHandler;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.InlineHTML;
+import com.google.gwt.user.client.ui.TreeItem;
 
 /**
  * Presenter for the ProductGroup page
@@ -8,14 +26,86 @@ import agilexs.catalogxsadmin.presentation.client.page.Presenter;
 public class ProductGroupPresenter implements Presenter<ProductGroupView> {
 
   private final ProductGroupView view = new ProductGroupView();
+  private CatalogView catalogView; //FIXME handle catalog view
+  private final HashMap<TreeItem, ProductGroup> treemap = new HashMap<TreeItem, ProductGroup>();
 
   public ProductGroupPresenter() {
+    view.getTree().addOpenHandler(new OpenHandler<TreeItem>() {
+      @Override
+      public void onOpen(OpenEvent<TreeItem> event) {
+        final TreeItem item = event.getTarget();
+
+        //FIXME: handle nodes that have no children in the database anyway
+        if (item.getChildCount() == 0) {
+          loadChildren(item);
+        }
+      }
+    });
+    view.getTree().addSelectionHandler(new SelectionHandler<TreeItem>() {
+      @Override
+      public void onSelection(SelectionEvent<TreeItem> event) {
+        final ProductGroup pg = treemap.get(event.getSelectedItem());
+
+        if (pg != null) {
+          loadGrid(pg);
+        }
+      }});
+    view.getNewButtonClickHandler().addClickHandler(
+        new ClickHandler() {
+          @Override
+          public void onClick(ClickEvent event) {
+            
+          }});
   }
 
   @Override
   public ProductGroupView getView() {
     return view;
   }
-  
-  
+
+  private void loadChildren(final TreeItem parent) {
+    CatalogServiceAsync.findAllProductGroupChildren(
+        catalogView, treemap.get(parent), new AsyncCallback<List<ProductGroup>>() {
+          @Override
+          public void onFailure(Throwable caught) {
+            //FIXME implement handling failure
+          }
+
+          @Override
+          public void onSuccess(List<ProductGroup> result) {
+            for (ProductGroup productGroup : result) {
+              treemap.put(
+                  view.addTreeItem(parent, productGroup.getName()), productGroup);
+            }
+          }
+        });
+  }
+
+  private void loadGrid(final ProductGroup pg) {
+    CatalogServiceAsync.findProductGroupById(pg.getId(),
+        new AsyncCallback<ProductGroup>() {
+          @Override
+          public void onFailure(Throwable caught) {
+            //FIXME implement handling failure
+          }
+
+          @Override
+          public void onSuccess(ProductGroup pg) {
+            view.setProductGroup(pg);
+            final List<PropertyValue> result = pg.getDefaultValues();
+            final Grid g = view.getGrid();
+
+            if (g.getRowCount() != result.size()) {
+              g.resizeRows(result.size());
+            }
+            //FIXME set header grid
+            for (int i = 0; i < result.size(); i++) {
+              PropertyValue propertyValue = result.get(i);
+              g.setWidget(i, 0, new InlineHTML(propertyValue.getProperty().getName()));
+            //  g.setWidget(i, 1, new InlineHTML(propertyValue.getDefaultValue());
+              g.setWidget(i, 2, new InlineHTML());
+            }
+          }
+    });
+  }
 }
--- CatalogXS/trunk/CatalogXSAdmin/source/agilexs/catalogxsadmin/presentation/client/ProductGroupView.java
+++ CatalogXS/trunk/CatalogXSAdmin/source/agilexs/catalogxsadmin/presentation/client/ProductGroupView.java
@@ -1,22 +1,76 @@
 package agilexs.catalogxsadmin.presentation.client;
 
+import agilexs.catalogxsadmin.presentation.client.binding.HasTextBinding;
+import agilexs.catalogxsadmin.presentation.client.catalog.ProductGroup;
+import agilexs.catalogxsadmin.presentation.client.catalog.ProductGroupBinding;
 import agilexs.catalogxsadmin.presentation.client.page.View;
 
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.SplitLayoutPanel;
+import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.Widget;
 
 public class ProductGroupView extends Composite implements View {
 
   FlowPanel panel = new FlowPanel();
+  final Tree tree = new Tree();
+  final Grid grid = new Grid();
+  final Label name = new Label();
+  final ProductGroupBinding pgBinding = new ProductGroupBinding();
+  final Button button = new Button("Add new product group");
   
   public ProductGroupView() {
     initWidget(panel);
-    
+    final SplitLayoutPanel slp = new SplitLayoutPanel();
+
+    panel.add(slp);
+    slp.addWest(tree, 300);
+    final FlowPanel detailPanel = new FlowPanel();
+
+    detailPanel.add(button);
+    detailPanel.add(name);
+    detailPanel.add(grid);
+    slp.add(detailPanel);
+
+    //Bindings
+    HasTextBinding.bind(name, pgBinding.name());
+  }
+
+  public Tree getTree() {
+    return tree;
+  }
+
+  public Grid getGrid() {
+    return grid;
   }
 
   @Override
   public Widget getViewWidget() {
     return this;
+  }
+
+  public void setProductGroup(ProductGroup pg) {
+    pgBinding.setData(pg);
+  }
+
+  HasClickHandlers getNewButtonClickHandler() {
+    return button;
+  }
+  
+  public TreeItem addTreeItem(TreeItem parent, String text) {
+    final TreeItem item = new TreeItem(text);
+
+    if (parent == null) {
+      tree.addItem(item);
+    } else {
+      parent.addItem(item);
+    }
+    return item;
   }
 }
