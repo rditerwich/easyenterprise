f2e1ba49572f1e00b00f3bb925777f68993f0dd5
ruud.diterwich <ruud.diterwich@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2010-05-29 07:18:57 +0000

    git-svn-id: file:///var/svn/easyenterprise@237 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- CatalogXS/trunk/CatalogXS/source/agilexs/catalogxs/presentation/cache/BasicCache.scala
+++ CatalogXS/trunk/CatalogXS/source/agilexs/catalogxs/presentation/cache/BasicCache.scala
@@ -10,7 +10,6 @@ import agilexs.catalogxs.businesslogic.CatalogBean
 class BasicCache[A] {
 
     //this annotation doesn't work....
-  @EJB{val name = "ejb/CatalogBean"} private[this] var catalogBean : agilexs.catalogxs.businesslogic.Catalog = _
 
   def lookupCatalog() : agilexs.catalogxs.businesslogic.Catalog = {
     val ic = new InitialContext()
