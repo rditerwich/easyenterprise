deefc7b9787e892dda73f9472018366d562d1669
ruud.diterwich <ruud.diterwich@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2010-10-15 05:15:12 +0000

    git-svn-id: file:///var/svn/easyenterprise@360 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- easyenterprise/ee-server/trunk/src/main/java/easyenterprise/server/EEServer.java
+++ easyenterprise/ee-server/trunk/src/main/java/easyenterprise/server/EEServer.java
@@ -1,7 +1,15 @@
 package easyenterprise.server;
 
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.servlet.ServletConfig;
+
+import org.eclipse.persistence.Version;
 
 import easyenterprise.lib.command.Command;
 import easyenterprise.lib.command.CommandException;
@@ -9,17 +17,41 @@ import easyenterprise.lib.command.CommandResult;
 import easyenterprise.lib.command.CommandService;
 import easyenterprise.lib.command.RegisteredCommands;
 import easyenterprise.lib.command.jpa.JpaCommandWrapper;
-import easyenterprise.server.account.impl.RegisteredAccountCommands;
+import easyenterprise.server.account.command.impl.RegisteredAccountCommands;
 
 public class EEServer implements CommandService {
 
 	private final RegisteredCommands registeredCommands = RegisteredCommands.create(new RegisteredAccountCommands());
 
-	private final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ee-persistenceunit");
-	private final JpaCommandWrapper jpaCommandWrapper = new JpaCommandWrapper(registeredCommands, entityManagerFactory );
+	private final EntityManagerFactory entityManagerFactory;
+	private final JpaCommandWrapper jpaCommandWrapper;
+	
+	public EEServer(ServletConfig config) {
+		this(collectProperties(config));
+	}
+
+	public EEServer(Map<String, String> properties) {
+		entityManagerFactory = Persistence.createEntityManagerFactory("ee-database", properties);
+		jpaCommandWrapper = new JpaCommandWrapper(registeredCommands, entityManagerFactory );
+	}
 	
 	@Override
 	public <T extends CommandResult, C extends Command<T>> T execute(C command) throws CommandException {
 		return jpaCommandWrapper.execute(command);
+	}
+	
+	private static Map<String, String> collectProperties(ServletConfig config) {
+		Map<String, String> properties = new HashMap<String, String>();
+		
+		for (@SuppressWarnings("unchecked")
+		Enumeration<String> e = config.getInitParameterNames(); e.hasMoreElements(); ) {
+			String name = e.nextElement();
+			properties.put(name, config.getInitParameter(name));
+		}
+		properties.putAll(System.getenv());
+		for (String name : System.getProperties().stringPropertyNames()) {
+			properties.put(name, System.getProperty(name));
+		}
+		return properties;
 	}
 }
