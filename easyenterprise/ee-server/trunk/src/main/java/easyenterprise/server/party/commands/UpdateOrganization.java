e3bc96337301bdfeda5530d252b7f8498b7b72cb
ruud.diterwich <ruud.diterwich@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2010-10-15 05:15:47 +0000

    git-svn-id: file:///var/svn/easyenterprise@361 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- easyenterprise/ee-server/trunk/src/main/java/easyenterprise/server/party/commands/UpdateOrganization.java
+++
@@ -1,9 +0,0 @@
-package easyenterprise.server.party.commands;
-
-import easyenterprise.server.party.Organization;
-
-public class UpdateOrganization extends UpdateParty<Organization, UpdateOrganization> {
-
-	private static final long serialVersionUID = 1L;
-
-}
--- easyenterprise/ee-server/trunk/src/main/java/easyenterprise/server/party/commands/UpdatePerson.java
+++
@@ -1,9 +0,0 @@
-package easyenterprise.server.party.commands;
-
-import easyenterprise.server.party.Person;
-
-public class UpdatePerson extends UpdateParty<Person, UpdatePerson> {
-
-	private static final long serialVersionUID = 1L;
-
-}
