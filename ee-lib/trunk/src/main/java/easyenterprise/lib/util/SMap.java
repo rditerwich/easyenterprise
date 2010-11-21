5dc9644516dd00b862aa5196ae4171f419caa786
ruud.diterwich <ruud.diterwich@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2010-11-21 10:48:32 +0000

    git-svn-id: file:///var/svn/easyenterprise@412 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- ee-lib/trunk/src/main/java/easyenterprise/lib/util/SMap.java
+++ ee-lib/trunk/src/main/java/easyenterprise/lib/util/SMap.java
@@ -1,14 +1,20 @@
 package easyenterprise.lib.util;
 
 import static com.google.common.base.Objects.equal;
+import static com.google.common.collect.Maps.immutableEntry;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
+import static java.util.Collections.singleton;
 import static java.util.Collections.singletonList;
 
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import com.google.common.base.Objects;
 
@@ -19,8 +25,10 @@ import com.google.common.base.Objects;
  * items have the same key, or when there is only one value per key.
  * Large content is inefficient, specially when building them. Class
  * is particularly useful for caching and transport purposes.   
+ * 
+ * All keys and values remain ordered according to order of insertion.
  */
-public abstract class SMap<K, V> implements Serializable {
+public abstract class SMap<K, V> implements Iterable<Entry<K, V>>, Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private static SMap<?, ?> emptyMap = new Empty<Object, Object>(); 
@@ -43,7 +51,7 @@ public abstract class SMap<K, V> implements Serializable {
 	 * Use {@link #getKeys()} to make the distinction. 
 	 * @return Key or null.
 	 */
-	public abstract K getKey();
+	public abstract K getFirstKey();
 
 	/**
 	 * Returns the keys for which the map has values. There
@@ -56,18 +64,26 @@ public abstract class SMap<K, V> implements Serializable {
 	/**
 	 * Returns the first value of the first key. Note the null is
 	 * returned either when the first value of the first key is null
-	 * or when the map is empty. Use {@link #getValues()} to make
+	 * or when the map is empty. Use {@link #getFirstValues()} to make
 	 * the distiction.
 	 * @return Value or null.
 	 */
-	public abstract V getValue();
+	public abstract V getFirstValue();
 	
 	/**
 	 * Returns the values corresponding to the first key. The empty
 	 * list will be returned only when the map is empty.
 	 * @return Values or the empty list.
 	 */
-	public abstract List<V> getValues();
+	public abstract List<V> getFirstValues();
+	
+	/**
+	 * Returns the value corresponding to the null key.
+	 * @return Value or null.
+	 */
+	public V getValue() {
+		return getValue(null);
+	}
 	
 	/**
 	 * Returns the first value for given key. Note the null is
@@ -78,6 +94,14 @@ public abstract class SMap<K, V> implements Serializable {
 	 * @return Value or null.
 	 */
 	public abstract V getValue(K key);
+	
+	/**
+	 * Returns the values corresponding to the null key.
+	 * @return Values or the empty list
+	 */
+	public List<V> getValues() {
+		return getValues(null);
+	}
 	
 	/**
 	 * Returns the values for given key. 
@@ -119,6 +143,14 @@ public abstract class SMap<K, V> implements Serializable {
 		if (values.size() == 1) return add(key, values.iterator().next());
 		return doAddAll(key, values);
 	}
+	
+	public SMap<K, V> addAll(Map<K, V> values) {
+		SMap<K, V> map = this;
+		for (Entry<K, V> entry : values.entrySet()) {
+			map = map.add(entry.getKey(), entry.getValue());
+		}
+		return map;
+	}
 
 	/**
 	 * It's guaranteed that values.size() > 1 
@@ -153,24 +185,27 @@ public abstract class SMap<K, V> implements Serializable {
 
 class Empty<K, V> extends SMap<K, V> {
 	private static final long serialVersionUID = 1L;
-	public K getKey() {
+	public K getFirstKey() {
 	  return null;
 	}
 	public List<K> getKeys() {
 		return emptyList();
 	}
-	public V getValue() {
+	public V getFirstValue() {
 		return null;
 	}
 	public V getValue(K key) {
 		return null;
 	};
 	@Override
-	public List<V> getValues() {
+	public List<V> getFirstValues() {
 		return emptyList();
 	}
 	public List<V> getValues(K key) {
 		return emptyList();
+	}
+	public Iterator<Entry<K, V>> iterator() {
+	  return Collections.<Entry<K, V>>emptyList().iterator();
 	}
 	public SMap<K, V> add(K key, V value) {
 		if (key == null) return new NoKeySingleValue<K, V>(value);
@@ -189,25 +224,28 @@ class NoKeySingleValue<K, V> extends SMap<K, V> {
 	public NoKeySingleValue(V value) {
 		this.value = value;
 	}
-	public K getKey() {
+	public K getFirstKey() {
 	  return null;
 	}
 	@SuppressWarnings("unchecked")
   public List<K> getKeys() {
 		return (List<K>) nullList;
 	}
-	public V getValue() {
+	public V getFirstValue() {
 		return value;
 	}
 	public V getValue(K key) {
 		return key == null ? value : null;
 	}
-	public List<V> getValues() {
+	public List<V> getFirstValues() {
 	  return singletonList(value);
 	}
 	public List<V> getValues(K key) {
 		if (key != null) return emptyList();
 		return singletonList(value);
+	}
+	public Iterator<Entry<K, V>> iterator() {
+	  return singleton(immutableEntry((K) null, value)).iterator();
 	}
 	public SMap<K, V> add(K key, V value) {
 		if (key == null) return new NoKeyMultiValue<K, V>(this.value, value); 
@@ -227,25 +265,28 @@ class SingleKeySingleValue<K, V> extends SMap<K, V> {
 		this.key = key;
 		this.value = value;
 	}
-	public K getKey() {
+	public K getFirstKey() {
 	  return key;
 	}
 	public List<K> getKeys() {
 		return singletonList(key);
 	}
-	public V getValue() {
+	public V getFirstValue() {
 	  return value;
 	}
 	public V getValue(K key) {
 		if (equal(this.key, key)) return value;
 		else return null;
 	}
-	public List<V> getValues() {
+	public List<V> getFirstValues() {
 		return singletonList(value);
 	}
 	public List<V> getValues(K key) {
 		if (equal(this.key, key)) return singletonList(value);
 		else return emptyList();
+	}
+	public Iterator<Entry<K, V>> iterator() {
+	  return singleton(immutableEntry(key, value)).iterator();
 	}
 	public SMap<K, V> add(K key, V value) {
 		if (equal(this.key, key)) return new SingleKeyMultiValue<K, V>(key, this.value, value); 
@@ -266,7 +307,7 @@ class MultiKeySingleValue<K, V> extends SMap<K, V> {
 		this.values = values;
 	}
 	@SuppressWarnings("unchecked")
-	public K getKey() {
+	public K getFirstKey() {
 	  return (K) keys[0];
 	}
 	@SuppressWarnings("unchecked")
@@ -274,7 +315,7 @@ class MultiKeySingleValue<K, V> extends SMap<K, V> {
 		return (List<K>) asList(keys);
 	}
 	@SuppressWarnings("unchecked")
-  public V getValue() {
+  public V getFirstValue() {
 	  return (V) values[0];
 	}
 	@SuppressWarnings("unchecked")
@@ -287,7 +328,7 @@ class MultiKeySingleValue<K, V> extends SMap<K, V> {
 		return null;
 	}
 	@SuppressWarnings("unchecked")
-	public List<V> getValues() {
+	public List<V> getFirstValues() {
 		return (List<V>) asList(values[0]);
 	}
 	@SuppressWarnings("unchecked")
@@ -298,6 +339,21 @@ class MultiKeySingleValue<K, V> extends SMap<K, V> {
 			}
 		}
 		return emptyList();
+	}
+	public Iterator<Entry<K, V>> iterator() {
+		return new Iterator<Map.Entry<K,V>>() {
+			private int index = 0;
+      public boolean hasNext() {
+	      return index < values.length;
+      }
+      @SuppressWarnings("unchecked")
+      public Entry<K, V> next() {
+	      return immutableEntry((K) keys[index], (V) values[index++]);
+      }
+      public void remove() {
+      	throw new UnsupportedOperationException();
+      }
+		};
 	}
 	public SMap<K, V> add(K key, V value) {
 		for (int i = 0; i < keys.length; i++) {
@@ -331,7 +387,7 @@ class NoKeyMultiValue<K, V> extends SMap<K, V> {
 	public NoKeyMultiValue(Object... values) {
 		this.values = values;
 	}
-	public K getKey() {
+	public K getFirstKey() {
 	  return null;
 	}
 	@SuppressWarnings("unchecked")
@@ -339,7 +395,7 @@ class NoKeyMultiValue<K, V> extends SMap<K, V> {
 		return (List<K>) nullList;
 	}
 	@SuppressWarnings("unchecked")
-  public V getValue() {
+  public V getFirstValue() {
 	  return (V) values[0];
 	}
 	@SuppressWarnings("unchecked")
@@ -347,13 +403,28 @@ class NoKeyMultiValue<K, V> extends SMap<K, V> {
 		return (V) (key == null ? values[0] : null);
 	};
 	@SuppressWarnings("unchecked")
-	public List<V> getValues() {
+	public List<V> getFirstValues() {
 		return (List<V>) asList(values);
 	}
 	@SuppressWarnings("unchecked")
 	public List<V> getValues(K key) {
 		if (key != null) return emptyList();
 		return (List<V>) asList(values);
+	}
+	public Iterator<Entry<K, V>> iterator() {
+		return new Iterator<Map.Entry<K,V>>() {
+			private int index = 0;
+      public boolean hasNext() {
+	      return index < values.length;
+      }
+      @SuppressWarnings("unchecked")
+      public Entry<K, V> next() {
+	      return immutableEntry((K) null, (V) values[index++]);
+      }
+      public void remove() {
+      	throw new UnsupportedOperationException();
+      }
+		};
 	}
 	public SMap<K, V> add(K key, V value) {
 		if (key == null)
@@ -377,14 +448,14 @@ class SingleKeyMultiValue<K, V> extends SMap<K, V> {
 		this.key = key;
 		this.values = values;
 	}
-	public K getKey() {
+	public K getFirstKey() {
 		return key;
 	}
 	public List<K> getKeys() {
 		return singletonList(key);
 	}
 	@SuppressWarnings("unchecked")
-	public V getValue() {
+	public V getFirstValue() {
 		return (V) values[0];
 	}
 	@SuppressWarnings("unchecked")
@@ -392,13 +463,28 @@ class SingleKeyMultiValue<K, V> extends SMap<K, V> {
 		return (V) (key == null ? values[0] : null);
 	};
 	@SuppressWarnings("unchecked")
-	public List<V> getValues() {
+	public List<V> getFirstValues() {
 		return (List<V>) asList(values);
 	}
 	@SuppressWarnings("unchecked")
 	public List<V> getValues(K key) {
 		if (!equal(this.key, key)) return emptyList();
 		return (List<V>) asList(values);
+	}
+	public Iterator<Entry<K, V>> iterator() {
+		return new Iterator<Map.Entry<K,V>>() {
+			private int index = 0;
+      public boolean hasNext() {
+	      return index < values.length;
+      }
+      @SuppressWarnings("unchecked")
+      public Entry<K, V> next() {
+	      return immutableEntry(key, (V) values[index++]);
+      }
+      public void remove() {
+      	throw new UnsupportedOperationException();
+      }
+		};
 	}
 	public SMap<K, V> add(K key, V value) {
 		if (equal(this.key, key))
@@ -423,7 +509,7 @@ class MultiKeyMultiValue<K, V> extends SMap<K, V> {
 		this.values = values;
 	}
 	@SuppressWarnings("unchecked")
-	public K getKey() {
+	public K getFirstKey() {
 	  return (K) keys[0];
 	}
 	@SuppressWarnings("unchecked")
@@ -431,7 +517,7 @@ class MultiKeyMultiValue<K, V> extends SMap<K, V> {
 		return (List<K>) asList(keys);
 	}
 	@SuppressWarnings("unchecked")
-  public V getValue() {
+  public V getFirstValue() {
 	  return (V) values[0][0];
 	}
 	@SuppressWarnings("unchecked")
@@ -443,7 +529,7 @@ class MultiKeyMultiValue<K, V> extends SMap<K, V> {
 		return null;
 	}
 	@SuppressWarnings("unchecked")
-	public List<V> getValues() {
+	public List<V> getFirstValues() {
 		return (List<V>) asList(values[0]);
 	}
 	@SuppressWarnings("unchecked")
@@ -453,6 +539,23 @@ class MultiKeyMultiValue<K, V> extends SMap<K, V> {
 				return (List<V>) asList(values[i]);
 		}
 		return emptyList();
+	}
+	public Iterator<Entry<K, V>> iterator() {
+		return new Iterator<Map.Entry<K,V>>() {
+			private int k = 0;
+			private int v = 0;
+      public boolean hasNext() {
+      	if (v >= values.length) { k++; v = 0; }
+	      return k < keys.length;
+      }
+      @SuppressWarnings("unchecked")
+      public Entry<K, V> next() {
+	      return immutableEntry((K) keys[k], (V) values[k][v++]);
+      }
+      public void remove() {
+      	throw new UnsupportedOperationException();
+      }
+		};
 	}
 	public SMap<K, V> add(K key, V value) {
 		for (int i = 0; i < keys.length; i++) {
