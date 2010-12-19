d474a56bb6f38823564257f2ed05766e07f1e0c6
ruud.diterwich <ruud.diterwich@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2010-12-19 21:25:33 +0000

    git-svn-id: file:///var/svn/easyenterprise@458 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- ee-lib/trunk/src/main/java/easyenterprise/lib/util/CollectionUtil.java
+++ ee-lib/trunk/src/main/java/easyenterprise/lib/util/CollectionUtil.java
@@ -1,5 +1,6 @@
 package easyenterprise.lib.util;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -11,5 +12,19 @@ public class CollectionUtil {
 	
 	public static <T> Collection<T> notNull(Collection<T> collection) {
 		return collection == null ? Collections.<T>emptyList() : collection;
+	}
+	
+	public static <T> Collection<T> concat(Collection<T> c1, Collection<T> c2) {
+		if (c1 == null && c2 == null) return Collections.emptyList();
+		if (c1 == null) return c2;
+		if (c2 == null) return c1;
+		ArrayList<T> list = new ArrayList<T>(c1.size() + c2.size());
+		list.addAll(c1);
+		list.addAll(c2);
+		return list;
+	}
+	
+	public static <T> T first(Iterable<T> iterable) {
+		return iterable.iterator().next();
 	}
 }
