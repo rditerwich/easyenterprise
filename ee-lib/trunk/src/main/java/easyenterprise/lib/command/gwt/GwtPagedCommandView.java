fafa9bc906d075158a0a6f1229d379bd6f7bf411
ruud.diterwich <ruud.diterwich@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2011-02-06 13:27:59 +0000

    git-svn-id: file:///var/svn/easyenterprise@501 0db79848-0afd-4259-9b79-6da5ec97fbb6

--- ee-lib/trunk/src/main/java/easyenterprise/lib/command/gwt/GwtPagedCommandView.java
+++ ee-lib/trunk/src/main/java/easyenterprise/lib/command/gwt/GwtPagedCommandView.java
@@ -7,20 +7,31 @@ import java.util.List;
 public class GwtPagedCommandView<T> implements Iterable<T> {
 
 	private final int viewSize;
-	private final int maxViewSize;
+	private final int defaultFetchSize;
+	private final int expandLimit;
 	private int startIndex;
 	private List<T> results = new ArrayList<T>();
 	
 	public GwtPagedCommandView(int viewSize) {
 		this.viewSize = viewSize;
-		this.maxViewSize = viewSize * 2;
+		this.defaultFetchSize = viewSize * 2;
+		this.expandLimit = viewSize / 3;
 	}
 	
 	public T getResult(int index) {
-		if (index < startIndex + results.size()) {
+		int endIndex = startIndex + results.size();
+		if (index < endIndex) {
 			return results.get(index - startIndex);
 		}
+		int fetchStart = index;
+		int fetchSize = this.defaultFetchSize;
+		if (index - endIndex <= expandLimit) {
+			fetchStart = endIndex;
+		} else if (startIndex - (index + fetchSize) <= expandLimit) {
+			fetchSize = startIndex - index;
+		}
 		startIndex = index;
+		
 		return null;
 	}
 	
