e5e780d482768486546a5c91cbcdc18409be5b9f
ruud.diterwich <ruud.diterwich@0db79848-0afd-4259-9b79-6da5ec97fbb6>
2011-02-08 13:49:09 +0000

    git-svn-id: file:///var/svn/easyenterprise@506 0db79848-0afd-4259-9b79-6da5ec97fbb6

---
+++ ee-lib/trunk/src/main/java/easyenterprise/lib/jpa/AbstractDao.java
@@ -0,0 +1,76 @@
+package easyenterprise.lib.jpa;
+
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class AbstractDao {
+
+	private final EntityManagerFactory factory;
+
+	private final ThreadLocal<EntityManager> entityManager = new ThreadLocal<EntityManager>();
+	
+	protected AbstractDao(String persistenceUnitName, Map<String, String> properties) {
+		this.factory = Persistence.createEntityManagerFactory(persistenceUnitName, properties);
+	}
+
+	public EntityManagerFactory getEntityManagerFactory() {
+		return factory;
+	}
+	
+	public EntityManager getEntityManager() {
+		EntityManager em = entityManager.get();
+		if (em == null) {
+			em = factory.createEntityManager();
+		}
+		if (!em.getTransaction().isActive()) {
+			em.getTransaction().begin();
+		}
+		return em;
+	}
+	
+	public EntityManager startTransaction() {
+		return startTransaction(null);
+	}
+	
+	/**
+	 * Starts a transaction explicitly. A transaction is started implicitly
+	 * when {@link #getEntityManager()} is called.
+	 * @param properties
+	 * @return
+	 */
+	public EntityManager startTransaction(Map<String, String> properties) {
+		EntityManager em = entityManager.get();
+		if (em != null && em.getTransaction().isActive()) {
+			em.getTransaction().commit();
+		}
+		if (properties != null) {
+			em = factory.createEntityManager(properties);
+		} else {
+			em = factory.createEntityManager();
+		}
+		em.getTransaction().begin();
+		entityManager.set(em);
+		return em;
+	}
+	
+	public void commitTransaction() {
+		EntityManager em = entityManager.get();
+		if (em.getTransaction().isActive()) {
+			em.getTransaction().commit();
+		}
+		em.close();
+		entityManager.set(null);
+	}
+	
+	public void rollbackTransaction() {
+		EntityManager em = entityManager.get();
+		if (em.getTransaction().isActive()) {
+			em.getTransaction().rollback();
+		}
+		em.close();
+		entityManager.set(null);
+	}
+}
